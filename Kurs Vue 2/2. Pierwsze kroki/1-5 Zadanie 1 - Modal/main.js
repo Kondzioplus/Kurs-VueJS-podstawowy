90a4dbc12b69e212154336a3ff6c0f55644ce38b
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-24 14:04:28 +0200

    Zadanie 1 - Modal

---
+++ Kurs Vue 2/2. Pierwsze kroki/1-5 Zadanie 1 - Modal/main.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data: {
+       buttonStyle: 'display:block;',
+       modalStyle: 'display:none;',
+       header: 'Witaj świecie',
+       content: 'Hej, <a href="#">Tomek</a>!'
+    },
+    methods: {
+       openModal: function(){
+        this.buttonStyle = this.modalStyle;
+        this.modalStyle = 'display:flex;';
+       },
+       hideModal: function(){
+        this.buttonStyle = 'display:block;';
+        this.modalStyle = 'display:none;';
+       }
+    }
+})
