cba32d5e6da5a060d49d3a4281b83bf9f4547b2d
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-26 14:04:54 +0200

    Wykorzystanie cyklu życia instancji VueJS

--- Kurs Vue 2/3. Projekt - mistrz słówek/Projekt/main.js
+++ Kurs Vue 2/3. Projekt - mistrz słówek/Projekt/main.js
@@ -5,13 +5,23 @@ new Vue({
    answer: '',
    words: [],
    dictionary: [],
-   currentwordId: 0,
+   currentWordId: 0,
    currentWord: '',
   },
   methods:{
     // metoda obliczająca wynik
-    randWord: function(){
+    randWord: function(restrict){
       this.currentWordId = Math.floor(Math.random() * this.dictionary.length);
+      if(restrict !== undefined && this.dictionary.length >= 2){
+        if(randedId == restrict){
+          if(randedId > 0){
+            randedId--;
+          } else{
+            randedId = Math.round((Math.random() * this.dictionary.length-2)+1);
+          }
+        }
+      }
+      this.currentWordId = randedId;
       this.currentWord = this.dictionary[this.currentWordId][1];
     },
     // metoda inicjująca dane startowe
@@ -23,17 +33,23 @@ new Vue({
       ];
       this.words = [];
       this.result = 0;
+      this.randWord();
     },
     // metoda sprawdzająca poprawność odpowiedzi
     checkAnswer: function(){
       if(this.answer.trim().toLowerCase() == this.dictionary[this.currentWordId][0]){
         this.result += 1;
-        this.dictionary.splice(this.currentWordId, 1);
+        this.words.push(this.dictionary.splice(this.currentWordId, 1)[0]);
         this.randWord();
       } else{
         this.result -= 0.5;
+        this.randWord(this.currentWordId);
       }
       this.answer = '';
     }
+  },
+  created: function(){
+    this.initialState();
+    
   }
 });
