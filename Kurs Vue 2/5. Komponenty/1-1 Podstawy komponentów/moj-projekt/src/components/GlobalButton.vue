0968b2313b8aa2eca5f426f85c0183dbfa4a8f01
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-26 21:12:26 +0200

    Podstawy komponentów

---
+++ Kurs Vue 2/5. Komponenty/1-1 Podstawy komponentów/moj-projekt/src/components/GlobalButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="counter++">{{ counter }}</button>
+</template>
+
+<script>
+    export default{
+        data: () => ({
+            counter: 0
+        })
+    }
+</script>
