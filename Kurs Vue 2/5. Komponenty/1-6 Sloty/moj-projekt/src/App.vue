1f32c416b3a8e105e194ffe29cfb59b6f5e91591
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-28 16:30:13 +0200

    Sloty

---
+++ Kurs Vue 2/5. Komponenty/1-6 Sloty/moj-projekt/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <BootstrapPanel>
+      <template slot-scope="{ superhero }">
+        <template slot="header">Hej!!!</template>
+      Hej co słychać?
+      {{ color }} {{ superhero }}
+      </template>
+    </BootstrapPanel>
+  </div>
+</template>
+
+<script>
+import BootstrapPanel from "./components/BootstrapPanel";
+
+export default {
+  name: "App",
+  components: {
+    BootstrapPanel
+  },
+  data: () => ({
+    color: 'niebieski',
+  })
+}
+  
+</script>
+  
+<style>
+  #app {
+  padding: 20px;
+}
+</style>
---
+++ Kurs Vue 2/5. Komponenty/1-6 Sloty/moj-projekt/src/components/BootstrapPanel.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- eslint-disable -->
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h5 class="panel-title">
+                <slot name="header">Przywitanie</slot>
+            </h5>
+        </div>
+        <div class="panel-body">
+            <slot :superhero="superhero"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+    superhero: 'batman',
+  })
+}
+   
+           
+</script>
+
+<style scoped>
+    
+</style>
