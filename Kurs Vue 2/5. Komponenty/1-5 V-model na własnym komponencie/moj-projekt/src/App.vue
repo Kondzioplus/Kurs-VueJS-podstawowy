703bd70a0aa5f352509b11b3b904eaf1fda07d24
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-28 15:57:14 +0200

    V-model na własnym komponencie

---
+++ Kurs Vue 2/5. Komponenty/1-5 V-model na własnym komponencie/moj-projekt/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    {{ color }}
+    <CustomControl v-model="color"></CustomControl>   
+  </div>
+</template>
+
+<script>
+import CustomControl from "./components/CustomControl";
+
+  export default {
+    name: 'app',
+    components: {
+      CustomControl
+    },
+    data: () => ({
+      color: '#428bca'
+    })
+  }
+  
+</script>
+
+<style>
+#app {
+  padding: 20px;
+}
+</style>
---
+++ Kurs Vue 2/5. Komponenty/1-5 V-model na własnym komponencie/moj-projekt/src/components/CustomControl.vue
@@ -0,0 +1,75 @@
+<template>
+    <!-- eslint-disable -->
+    <div class="control">
+        <div class="picks">
+            <div 
+            class="pick"
+            v-for="color in freeColors"
+            :key="color"
+            :style="{backgroundColor: color}"
+            @click="updateModel(color)"></div>
+        </div>
+        <div class="current" :style="{backgroundColor: value}">
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        data: () => ({
+            colorsList: [
+                '#428bca',
+                '#5cb85c',
+                '#f0ad4e',
+                '#d9534f',
+                '#5bc0de',
+            ]
+        }),
+        props: {
+            value: String
+        },
+        computed: {
+            freeColors(){
+                return this.colorsList.filter(v => v !== this.value);
+            }
+        },
+        methods: {
+            updateModel(color){
+                this.$emit('input', color);
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .control {
+        width: 200px;
+        height: 100px;
+        border: 6px solid #333;
+        position: relative;
+        display: flex;
+    }
+
+    .control > .picks {
+        width: 50%;
+        height: 100%;   
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .control > .picks >.pick {
+        width: 50%;
+        height: 50%;
+        border: 5px solid rgba(0, 0, 0, 0.4);
+        cursor: pointer;
+    }
+
+    .control > .current {
+        width: 50%;
+        height: 100%;
+        border: 5px solid rgba(0, 0, 0, 0.4);
+        transition: 1s;
+    }   
+</style>
