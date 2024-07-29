ba0b4eb518752387dcf2221c75b9d1f8efb43a7f
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-29 18:27:16 +0200

    Wstęp do animacji

---
+++ Kurs Vue 2/7. Animowane przejścia/1-1 Wstęp do animacji/moj-projekt/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>Transition</h1>
+    <button class="btn btn-primary"  @click="display = !display">Przełącz</button>
+    <hr>
+    <transition name="fade" mode="out-in">
+      <div class="alert alert-success" key="1" v-if="display">
+      Witaj!
+    </div>
+    <div class="alert alert-info" key="2" v-else>
+      Witaj!
+    </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+    data:() => ({
+      display: false
+    })
+}
+  
+</script>
+  
+<style>
+  #app {
+  padding: 20px;
+}
+
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-leave-active {
+  transition: opacity 1s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
