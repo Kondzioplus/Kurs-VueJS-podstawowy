69ac693c68f696de3b2f03d2438b59bf35dee174
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-30 15:15:53 +0200

    beforeLeave Guard

---
+++ Kurs Vue 2/8. Vue-router/1-9 beforeLeave Guard/moj-projekt/src/components/AuthorDetails.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- eslint-disable -->
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            Panel autora
+        </div>
+        <div class="panel-body">
+            Autor to <b>{{ name }}</b> <br>
+            <router-link :to="'/author/' + $route.params.name + '/edit?lang=pl'" tag="button" class="btn btn-primary">Edytuj</router-link> &nbsp;
+            <router-link :to="'/author/' + $route.params.name + '/edit?lang=en'" tag="button" class="btn btn-primary">Edit</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props:{
+        name: String
+    }
+}
+</script>
