7f6d5c35a5d74f134c2cc790490bc0ab90452ca5
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-30 09:58:43 +0200

    Ręczna nawigacja I parametry routingu

---
+++ Kurs Vue 2/8. Vue-router/1-3 Ręczna nawigacja I parametry routingu/moj-projekt/src/components/TheNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+<ul class="nav nav-pills">
+    <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
+    <router-link tag="li" active-class="active" to="/about"><a>About</a></router-link>
+    <router-link tag="li" active-class="active" to="/author/Konrad"><a>Author A</a></router-link>
+    <router-link tag="li" active-class="active" to="/author/Adam"><a>Author B</a></router-link>
+</ul>
+    
+</template>
+
+<script>
+
+</script>
+
---
+++ Kurs Vue 2/8. Vue-router/1-3 Ręczna nawigacja I parametry routingu/moj-projekt/src/views/AuthorView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="author">
+    <h1>Młody programista VueJS - {{ name }}</h1>
+    <button class="btn btn-success" @click="backToHome">Wróc na stronę główną</button>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+  export default {
+    /*data(){
+      return {
+        name: this.$route.params.name
+      };
+    },
+    watch: {
+      '$route'(to, from){
+        this.name = to.params.name;
+      }
+    },*/
+    props:{
+      name: {
+        type: String,
+      }
+    },
+
+    methods: {
+      backToHome() {
+        // this.$router.push('/')
+        this.$router.replace({name: 'home'});
+      }
+    }
+  }
+</script>
