1bac2dd5d82f412a2e6ce40265cc49e91b5a7a00
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-07-30 14:45:57 +0200

    Przekierowania I dopasowywanie dynamiczne

---
+++ Kurs Vue 2/8. Vue-router/1-7 Przekierowania I dopasowywanie dynamiczne/moj-projekt/src/components/AuthorEdit.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- eslint-disable -->
+    <div>
+        <h4>{{ header }}</h4>
+        <hr>
+        <router-link :to="{ name: 'author-name', params: { name: $route.params.name } }" tag="button" class="btn btn-primary">
+          {{ btn }}
+        </router-link>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default{
+    props:{
+        name: String
+    },
+    computed: {
+        lang(){
+            return this.$route.query.lang;
+        },
+        header(){
+            return this.lang == 'pl' ? 'Edycja autora' : 'Author\'s editing';
+        },
+        btn(){
+            return this.lang == 'pl' ? 'Powrót' : 'Back';
+        }
+    }
+}
+</script>
